e72a6023c0934ffeb2dfb9da38fad5acaaecd19e
Jeremy Dion <dionjeremy7@gmail.com>
2021-02-17 15:27:58 -0500

    autre commit

--- dame/src/main/java/dame/pages/Profil/PartieProfil.java
+++ dame/src/main/java/dame/pages/Profil/PartieProfil.java
@@ -3,15 +3,13 @@ package dame.pages.Profil;
 
 
 import java.io.IOException;
-import java.util.Random;
-
-import modeles.Profil;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import ntro.debogage.J;
 import ntro.javafx.Initialisateur;
-import ntro.mvc.modeles.EntrepotDeModeles;
 
 
-public class PartieProfil  {
+public class PartieProfil extends Application  {
 	
 	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03"};
 	
@@ -26,10 +24,18 @@ public class PartieProfil  {
 	public static void main(String []args) throws IOException {
 		J.appel(PartieProfil.class);
 		
-		
+		System.out.print("test");
 		
 		
 	
+		
+	}
+
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 	
