daea119d257be7f300a3c3834f9de53dc2125220
Jeremy Dion <dionjeremy7@gmail.com>
2021-02-17 15:08:57 -0500

    module 3

--- dame/src/main/java/dame/pages/Profil/PartieProfil.java
+++ dame/src/main/java/dame/pages/Profil/PartieProfil.java
@@ -1,8 +1,40 @@
 package dame.pages.Profil;
 
-public class Profil {
 
-	public static void main(String []args) {
-		System.out.println("Bonjour!");
+
+import java.io.IOException;
+import java.util.Random;
+
+import modeles.Profil;
+import ntro.debogage.J;
+import ntro.javafx.Initialisateur;
+import ntro.mvc.modeles.EntrepotDeModeles;
+
+
+public class PartieProfil  {
+	
+	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03"};
+	
+	static {
+		Initialisateur.initialiser();
+		J.appel(PartieProfil.class);
+		
 	}
+	
+	
+	
+	public static void main(String []args) throws IOException {
+		J.appel(PartieProfil.class);
+		
+		
+		
+		
+	
+		
+	}
+	
+	
+	
+	
+	
 }
